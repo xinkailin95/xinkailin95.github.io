26f64004dfa2c11e24a0560adb9ec25988e48f78
Xinkai Lin <xinkaili@buffalo.edu>
2022-09-10 01:13:33 -0400

    vue init checkin

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome to my website</h1>
+  </div>
+  <router-link to="/">Home</router-link>
+</template>
+
+<script>
+  export default {
+    name: 'MyApp',
+  };
+</script>
+
+<style lang="less" scoped></style>
