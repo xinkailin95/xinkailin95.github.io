88b3016d8ac79bbd77420096204333df5129a2b1
Xinkai Lin <xinkaili@buffalo.edu>
2022-10-18 18:00:29 -0400

    add photo page

---
+++ js/422.60a34451.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkxinkailin95_github_io"]=self["webpackChunkxinkailin95_github_io"]||[]).push([[422],{8422:(n,i,a)=>{a.r(i),a.d(i,{default:()=>_});var e=a(3396),s=function(n){return(0,e.dD)("data-v-54013365"),n=n(),(0,e.Cn)(),n},t={class:"games"},u=s((function(){return(0,e._)("p",null,"This is games",-1)})),r=[u];function c(n,i,a,s,u,c){return(0,e.wg)(),(0,e.iD)("div",t,r)}const d={name:"MyGames"};var l=a(89);const o=(0,l.Z)(d,[["render",c],["__scopeId","data-v-54013365"]]),_=o}}]);
+//# sourceMappingURL=422.60a34451.js.map
